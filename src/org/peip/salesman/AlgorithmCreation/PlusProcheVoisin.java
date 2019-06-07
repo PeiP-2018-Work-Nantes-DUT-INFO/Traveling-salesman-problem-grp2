39521f470eef6f5298a1e7ee2c525baa7603c569
Nathan ROCHER <nathan.rocher@etu.univ-nantes.fr>
2019-06-07 16:33:52 +0200

    Add algo

--- src/org/peip/salesman/AlgorithmCreation/PlusProcheVoisin.java
+++ src/org/peip/salesman/AlgorithmCreation/PlusProcheVoisin.java
@@ -1,6 +1,7 @@
 package org.peip.salesman.AlgorithmCreation;
 
 import org.peip.salesman.Chemin;
+import org.peip.salesman.GestionMatrice;
 
 public class PlusProcheVoisin {
 	/*
@@ -8,8 +9,11 @@ public class PlusProcheVoisin {
 	 * @param matriceDistance la matrice des distances entre villes
 	 * @param indexDepart la ville de départ du chemin
 	 */
-	public Chemin creerChemin(float[][] matriceDistance, int indexDepart) {
+	public static Chemin creerChemin(GestionMatrice matrice, int indexDepart) {
 		// TODO Algorithme du plus proche voisin
 		return null;
+	}
+
+	public static void creerChemin(GestionMatrice g) {
 	}
 }

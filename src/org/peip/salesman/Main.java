481c8be3dce7982d92d6a5342378bdbe5f309162
Nathan ROCHER <nathan.rocher@etu.univ-nantes.fr>
2019-06-07 15:04:48 +0200

    GROUPE 1

---
+++ src/org/peip/salesman/Main.java
@@ -0,0 +1,13 @@
+package org.peip.salesman;
+
+import org.peip.salesman.GestionMatrice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GestionMatrice g = new GestionMatrice();
+		System.out.println(g.toString());
+		System.out.println("Fin du programme..");
+	}
+
+}

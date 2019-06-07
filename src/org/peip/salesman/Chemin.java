b1f257ac578488f22d01cae58dfb242e49256e0e
mxb <mxb@mxb.com>
2019-06-07 20:15:43 +0200

    Fixed chemin

--- src/org/peip/salesman/Chemin.java
+++ src/org/peip/salesman/Chemin.java
@@ -2,7 +2,6 @@ package org.peip.salesman;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class Chemin extends ArrayList<Integer> {
     private int distancetotale;

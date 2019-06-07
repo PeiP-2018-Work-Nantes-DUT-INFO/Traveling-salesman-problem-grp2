2267bf0efe69673d0feb0b4796a3672ff959ee60
Eliott Dubois <eliott.dubois@gmail.com>
2019-06-07 16:31:31 +0200

    Algo angle plus ouvert

---
+++ src/org/peip/salesman/AlgorithmCreation/AnglePlusOuvert.java
@@ -0,0 +1,16 @@
+package org.peip.salesman.AlgorithmCreation;
+
+import org.peip.salesman.Chemin;
+
+public class AnglePlusOuvert {
+	/*
+	 * 
+	 * @param matriceDistance la matrice des distances entre villes
+	 * @param indexDepart la première ville de départ du chemin
+	 * @param indexBis la seconde ville de départ du chemin
+	 */
+	public static Chemin creerChemin(float[][] matriceDistance, int indexDepart, int indexBis) {
+		// TODO Algorithme du plus proche voisin
+		return null;
+	}
+}
